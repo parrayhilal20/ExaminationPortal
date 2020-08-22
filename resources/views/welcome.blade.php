0180a2edd52bfe6481713c47bc3f9db9d34fbd1d
parrayhilal20 <parayhilal13@gmail.com>
2020-08-22 16:29:37 +0530

    Change the Examination to Exam and  the System

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Online Examination Systems
+                    Online Exam System
                 </div>
             </div>
         </div>
